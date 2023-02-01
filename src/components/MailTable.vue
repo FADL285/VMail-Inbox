10ad95128765700dc2a0cfff07d5556311510937
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-02-01 21:16:03 +0200

    Add Change Mail Functionalities 🚀🚀
    ToggleRead functionality
    ToggleArchive functionality
    GoNewer and GoOlder functionality
    and more....

--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
+import { computed, ref } from "vue";
 import axios from "axios";
 import useFormatDate from "@/composables/useFormatDate";
 import MailView from "./MailView.vue";
@@ -16,24 +16,56 @@ const sortedEmails = computed(() =>
   [...emails.value].sort((a, b) => new Date(b.sentAt) - new Date(a.sentAt))
 );
 const unArchivedEmails = computed(() =>
-  sortedEmails.value.filter((mail) => !mail.archived)
+  sortedEmails.value.filter((email) => !email.archived)
 );
+const isLastEmail = computed(() => {
+  const index = unArchivedEmails.value.findIndex(
+    (e) => e.id === openedEmail.value?.id
+  );
+  return index === unArchivedEmails.value.length - 1;
+});
+const isFirstEmail = computed(() => {
+  const index = unArchivedEmails.value.findIndex(
+    (e) => e.id === openedEmail.value?.id
+  );
+  return index === 0;
+});
 
 // Methods
-const updateMail = async (mail) => {
-  await axios.put(`${import.meta.env.VITE_API_URL}/emails/${mail.id}`, mail);
+const updateEmail = async (email) => {
+  await axios.put(`${import.meta.env.VITE_API_URL}/emails/${email.id}`, email);
 };
-const archiveEmail = async (mail) => {
-  mail.archived = true;
-  await updateMail(mail);
+const markAsArchived = async (email) => {
+  email.archived = !email.archived;
+  await updateEmail(email);
 };
-const markEmailAsRead = async (mail) => {
-  mail.read = true;
-  await updateMail(mail);
+const markEmailAsRead = async (email) => {
+  email.read = true;
+  await updateEmail(email);
 };
-const openEmail = async (mail) => {
-  openedEmail.value = mail;
-  await markEmailAsRead(mail);
+const openEmail = async (email) => {
+  openedEmail.value = email;
+  await markEmailAsRead(email);
+};
+const changeEmail = async ({
+  toggleRead,
+  toggleArchive,
+  save,
+  closeModal,
+  changeIndex,
+}) => {
+  if (toggleRead) openedEmail.value.read = !openedEmail.value.read;
+  if (toggleArchive) openedEmail.value.archived = !openedEmail.value.archived;
+  if (save) await updateEmail(openedEmail.value);
+  if (changeIndex) {
+    if (isFirstEmail.value && changeIndex === -1) return;
+    if (isLastEmail.value && changeIndex === 1) return;
+    const index = unArchivedEmails.value.findIndex(
+      (e) => e.id === openedEmail.value.id
+    );
+    openedEmail.value = unArchivedEmails.value.at(index + changeIndex);
+  }
+  if (closeModal) openedEmail.value = null;
 };
 </script>
 
@@ -58,13 +90,18 @@ const openEmail = async (mail) => {
           </p>
         </td>
         <td class="date">{{ useFormatDate(new Date(email.sentAt)) }}</td>
-        <td><button @click.stop="archiveEmail(email)">Archive</button></td>
+        <td><button @click.stop="markAsArchived(email)">Archive</button></td>
       </tr>
     </tbody>
   </table>
 
   <ModalView v-if="openedEmail" @close="openedEmail = null">
-    <MailView :email="openedEmail" />
+    <MailView
+      :email="openedEmail"
+      :is-first-email="isFirstEmail"
+      :is-last-email="isLastEmail"
+      @change-email="changeEmail"
+    />
   </ModalView>
 </template>
 
--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,17 +1,71 @@
 <script setup>
 import { marked } from "marked";
 import useFormatDate from "@/composables/useFormatDate";
+import useKeyDown from "@/composables/useKeyDown.js";
 
 defineProps({
   email: {
     type: Object,
     required: true,
   },
+  isLastEmail: {
+    type: Boolean,
+    required: true,
+  },
+  isFirstEmail: {
+    type: Boolean,
+    required: true,
+  },
 });
+const emit = defineEmits(["changeEmail"]);
+
+const toggleRead = () => {
+  emit("change-email", { toggleRead: true, save: true });
+};
+const toggleArchive = () => {
+  emit("change-email", { toggleArchive: true, save: true, closeModal: true });
+};
+const goNewer = () => {
+  emit("change-email", { changeIndex: -1 });
+};
+const goOlder = () => {
+  emit("change-email", { changeIndex: 1 });
+};
+const goNewerAndArchive = () => {
+  emit("change-email", { changeIndex: -1, toggleArchive: true, save: true });
+};
+const goOlderAndArchive = () => {
+  emit("change-email", { changeIndex: 1, toggleArchive: true, save: true });
+};
+
+useKeyDown([
+  { key: "r", fn: toggleRead },
+  { key: "e", fn: toggleArchive },
+  { key: "k", fn: goNewer },
+  { key: "ArrowLeft", fn: goNewer },
+  { key: "j", fn: goOlder },
+  { key: "ArrowRight", fn: goOlder },
+  { key: "[", fn: goNewerAndArchive },
+  { key: "]", fn: goOlderAndArchive },
+]);
 </script>
 
 <template>
   <div class="email-display">
+    <div>
+      <button @click="toggleArchive">
+        {{ email.archived ? "Move to Inbox" : "Archive" }} <small>(e)</small>
+      </button>
+      <button @click="toggleRead">
+        {{ email.read ? "Mark as Unread" : "Mark as Read" }} <small>(r)</small>
+      </button>
+      <button @click="goNewer" :disabled="isFirstEmail">
+        Newer <small>k</small>
+      </button>
+      <button @click="goOlder" :disabled="isLastEmail">
+        Older <small>j</small>
+      </button>
+    </div>
     <h2 class="mb-0">
       <span class="mr-2">Subject:</span>
       <strong>{{ email.subject }}</strong>
